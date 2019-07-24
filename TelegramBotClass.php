4da32a0ba40cc0f578c09250452b0e59b54e1708
dyspace <yevhenii.kukhol@gmail.com>
2019-07-24 12:06:54 +0300

    sendMessage ready

--- TelegramBotClass.php
+++ TelegramBotClass.php
@@ -4,16 +4,38 @@ class TelegramBot {
 
     private $token = '592148368:AAEOCKL9iyi1BegRhkt1pPC8v8oLdMm68bw';
     private $default_url = 'https://api.telegram.org/bot';
+    private $errors = [];
 
     private function formation_url ($query, $params = []) {
         $url = $this->default_url . $this->token . '/' . $query;
         if( ! empty($params) ){
             $url .= "?" . http_build_query($params);
-        }
+        }   
         return $url;
     }
     
-    
+    public function getUpdates ($params = []) {
+        $url = $this->formation_url ('getUpdates', $params);
+
+        $c = curl_init($url);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        $response = json_decode(curl_exec($c));
+        return $response;
+    }
+
+    public function sendMessage ($params = []) {
+        if(! isset($params['chat_id']) or ! isset($params['text'])) {
+            $errors[] .= 'sendMessage params incorrectly';
+        }else{
+            $url = $this->formation_url ('sendMessage', $params);
+            $s = curl_init($url);
+            curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
+            $response = json_decode(curl_exec($s));
+            return $response;
+        }
+
+    }
+
 
 
 
--- init.php
+++ init.php
@@ -3,3 +3,13 @@
 include "TelegramBotClass.php";
 
 $tg = new TelegramBot;
+$response = $tg->getUpdates();
+$chat_id = $response->result[0]->message->chat->id;
+$params = [
+    'chat_id' => $chat_id,
+    'text' => 'kek',
+];
+
+$tg->sendMessage($params);
+
+
